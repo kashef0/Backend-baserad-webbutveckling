c8c6838b44b43962c386f90c06b278dad7c0fa36
M.kashef <kashef.sy@gmail.com>
2024-05-05 20:13:35 +0200

    error console

--- api-server/routes/authRoutes.js
+++ api-server/routes/authRoutes.js
@@ -63,7 +63,7 @@ router.post("/login", async(req, res) => {
         res.status(200).json({ message: "user logged in!", token: token });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "server error" });
+        res.status(500).json({ error });
     }
     
 });
