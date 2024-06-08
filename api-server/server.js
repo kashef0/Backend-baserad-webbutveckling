c2d9390be11f5da14dfc5ab926f0b32cc6637443
M.kashef <kashef.sy@gmail.com>
2024-06-08 12:36:10 +0200

    new try to fix server

--- api-server/server.js
+++ api-server/server.js
@@ -1,5 +1,3 @@
-// 
-
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
@@ -11,34 +9,33 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-
 app.use("/api", authRoutes);
 
+// Protect the /api/protected route with the authenticateToken middleware
 app.get("/api/protected", authenticateToken, (req, res) => {
-    res.json({message: "shyddad route! "});
+    res.json({message: "Protected route!"});
 });
 
+// Middleware to authenticate the token
 function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if(!token || token == null) {
-        res.status(401).json({message: "Not authorized for this token missing!"});
+    if (!token) {
+        return res.status(401).json({message: "Not authorized, token missing!"});
     } 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
-        if(err) {
-            return res.status(403).json({message: "not correct jwt"});
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(403).json({message: "Invalid token"});
         }
 
-        req.username = username;
-
+        req.user = user; // store user information in request
         next();
     });
 }
 
-const port = process.env.PORT | 3000;
-
+const port = process.env.PORT || 3000;
 
 app.get("/home", authenticateToken, (req, res) => {
     res.render("home");
@@ -47,13 +44,11 @@ app.get("/home", authenticateToken, (req, res) => {
 app.get("/login", (req, res) => {
     res.render("login");
 });
+
 app.get("/signup", (req, res) => {
     res.render("signup");
 });
 
-
-
-
 app.listen(port, () => {
-    console.log("server started on port " + port);
+    console.log("Server started on port " + port);
 });
